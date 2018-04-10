15e58cfb6d8c86f01106dfc94dc26bbb7847d1c2
Ana Marojevic <ana94ns@hotmail.com>
2018-04-11 00:35:31 +0200

    Set up

---
+++ Finder/app/src/main/java/ftn/tim2/finder/MainActivity.java
@@ -0,0 +1,13 @@
+package ftn.tim2.finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
