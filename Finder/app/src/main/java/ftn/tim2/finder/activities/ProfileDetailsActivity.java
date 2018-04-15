097cdb1f7781d3f69ea2b46e02e778e831b6a819
Ana Marojevic <ana94ns@hotmail.com>
2018-04-15 17:08:29 +0200

    Created rate dialog, #11

--- Finder/app/src/main/java/ftn/tim2/finder/activities/ProfileDetailsActivity.java
+++ Finder/app/src/main/java/ftn/tim2/finder/activities/ProfileDetailsActivity.java
@@ -1,16 +1,38 @@
 package ftn.tim2.finder.activities;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ftn.tim2.finder.R;
 
 public class ProfileDetailsActivity extends AppCompatActivity {
 
+    Dialog rateDialog;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_detail);
+        rateDialog = new Dialog(this);
+    }
+
+    public void showRatePopup(View v){
+        TextView closeRate;
+        Button rateBtn;
+        rateDialog.setContentView(R.layout.rate_popup);
+        closeRate = rateDialog.findViewById(R.id.close_rate);
+        rateBtn = rateDialog.findViewById(R.id.rate_submit);
+        closeRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rateDialog.dismiss();
+            }
+        });
+        rateDialog.show();
     }
 }
